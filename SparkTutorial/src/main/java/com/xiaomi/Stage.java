890cc4b92fd6223c91bd9a7bd140afe32674c86e
WangCong <1018957763@qq.com>
2019-10-26 18:05:31 +0800

    add basic word count

--- SparkTutorial/src/main/java/com/xiaomi/Stage.java
+++ SparkTutorial/src/main/java/com/xiaomi/Stage.java
@@ -1,8 +1,11 @@
 package com.xiaomi;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.Function;
+import scala.Tuple2;
 
 import java.util.Arrays;
 import java.util.List;
@@ -12,8 +15,15 @@ public class Stage {
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("Stage").setMaster("local[*]");
         JavaSparkContext jsc = new JavaSparkContext(conf);
-        List<String> data = Arrays.asList("user1 30 99", "user2 40 9999", "user3 25 99");
-        JavaRDD<String> parallelize = jsc.parallelize(data);
-        System.out.println(parallelize.collect());
+        String path = Stage.class.getClassLoader().getResource("README.md").getPath();
+        JavaRDD<String> input = jsc.textFile(path);
+        JavaRDD<String> words = input.flatMap(s -> Arrays.asList(s.split(" ")).iterator());
+        JavaPairRDD<String, Integer> wordPair = words.mapToPair(word -> new Tuple2<>(word, 1));
+        JavaPairRDD<String, Integer> counts = wordPair.reduceByKey((i1, i2) -> i1 + i2);
+        List<Tuple2<String, Integer>> results = counts.collect();
+        for (Tuple2<String, Integer> result : results) {
+            System.out.println(result._1() + ": " + result._2());
+        }
+        jsc.stop();
     }
 }
